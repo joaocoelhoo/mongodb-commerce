c00cd2eec21f61ceeda116c1d2c41405169bf762
Joao Coelho <joaopaulomelocoelho@gmail.com>
2022-09-14 20:32:12 -0300

    fix lint and add avaliacao in all documents

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,4 +1,5 @@
 db.produtos
     .find(
     { ingredientes: { $elemMatch: { $eq: "picles" } } },
-    { _id: false, nome: 1, ingredientes: 1, valoresNutricionais: { $slice: 3 } });
+    { _id: false, nome: 1, ingredientes: 1, valoresNutricionais: { $slice: 3 } },
+    );
